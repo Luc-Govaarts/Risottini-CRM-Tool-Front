4e9ffbd216bfd95454745c95553608822a3d0473
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-06-30 14:17:02 +0200

    routing, login, redux set up, appState slice & User slice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
+import Loading from "./Components/Loading";
+import MessageBox from "./Components/MessageBox";
+import Homepage from './Pages/Homepage'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectAppLoading } from "./store/appState/selectors";
+import { getUserWithStoredToken } from "./store/user/actions";
 
 function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectAppLoading);
+
+  useEffect(() => {
+    dispatch(getUserWithStoredToken());
+  }, [dispatch]);
+  
   return (
     <div className="App">
-      
+      <MessageBox />
+      {isLoading ? <Loading /> : null}
+      <Switch>
+        <Route path="/login" component={Signup}/>
+        <Route path="/signup" component={Login}/>
+        <Route exact path="/" component={Homepage}/>
+      </Switch>
     </div>
   );
 }
