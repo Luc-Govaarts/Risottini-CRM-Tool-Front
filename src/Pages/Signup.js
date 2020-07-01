ba99cbdeaed4c0e87b10f856adbffa3c0ce7e606
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-07-01 12:20:10 +0200

    added signup form, adds user succesfully to database

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { signUp } from "../store/user/actions";
+import { selectToken } from "../store/user/selectors";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+import { Col } from "react-bootstrap";
 
-export default function Signup() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function SignUp() {
+  const [name, set_name] = useState("");
+  const [email, set_email] = useState("");
+  const [password, set_password] = useState("");
+  const [phone, set_phone] = useState()
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (token !== null) {
+      history.push("/");
+    }
+  }, [token, history]);
+
+  function submitForm(event) {
+    event.preventDefault();
+
+    dispatch(signUp(name, email, password, phone));
+
+    set_email("");
+    set_password("");
+    set_name("");
+    set_phone()
+  }
+
+  return (
+    <Container>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <h1 className="mt-5 mb-5">Signup</h1>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            onChange={event => set_name(event.target.value)}
+            type="text"
+            placeholder="Enter name"
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            value={email}
+            onChange={event => set_email(event.target.value)}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            onChange={event => set_password(event.target.value)}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group> 
+        
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control
+            value={phone}
+            onChange={event => set_phone(event.target.value)}
+            type="phone"
+            placeholder="Phone"
+            required
+          />
+        </Form.Group> 
+
+        <Form.Group className="mt-5">
+          <Button variant="primary" type="submit" onClick={submitForm}>
+            Sign up
+          </Button>
+        </Form.Group>
+        <Link to="/login">Click here to log in</Link>
+      </Form>
+    </Container>
+  );
 }
