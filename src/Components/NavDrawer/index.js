8b86598c6ecd5bf05f6ef0469ff8a21e46ecb61b
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-07-08 17:30:31 +0200

    cleaning up

--- src/Components/NavDrawer/index.js
+++ src/Components/NavDrawer/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import MyAppBar from './MyAppBar'
 import MyDrawer from './MyDrawer'
 
--- src/Components/ReportCard.js
+++ src/Components/ReportCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardHeader, 
+import { Card,
     CardContent, Typography,
     Box } from '@material-ui/core'
 import moment from 'moment';
