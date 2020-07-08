91a71adc48e25fe134a9041507b0c67d56e55af3
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-07-08 11:55:28 +0200

    added ContactCard

---
+++ src/Components/ContactCard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Card, Typography, CardContent, Box, CardHeader} from '@material-ui/core'
+import moment from 'moment';
+import { red } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      
+    },
+    avatar: {
+        backgroundColor: red[500]
+    }
+}))
+
+export default function ContactCard(props) {
+    const classes = useStyles();
+    return (
+        <Card>
+            <CardHeader
+                avatar={
+                <Avatar aria-label="contact" className={classes.avatar}>
+                    C
+                </Avatar>
+                }
+                title={props.name}
+                subheader={props.function}
+            />
+            <CardContent>
+                <Typography variant="h6" component="p">
+                    <strong>Email: </strong>{props.email}
+                </Typography>
+                <Typography variant="h6" component="p">
+                    <strong>Telefoon: </strong> {props.phone}
+                </Typography>
+                <Typography textAlign='right' variant="captiontext">
+                    <em>Gecreëerd:</em> {moment(props.createdAt).format("DD MMM YYYY")
+                }</Typography>
+            </CardContent>
+        </Card>
+    )
+}
