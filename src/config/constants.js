38f1686218f8f3ce317532f519759edef4b46691
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-08-04 13:15:17 +0200

    Adjusting DB configs for staging branch

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 
-export const apiUrl = "http://localhost:4000" || "https://crispy-production.herokuapp.com";
+export const apiUrl = "https://crispy-risottini.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
