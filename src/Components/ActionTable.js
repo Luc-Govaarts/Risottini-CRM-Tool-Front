5b3734145b1f3f2c68ffe2265b3a3b67418e399a
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-08-14 14:16:44 +0200

    Basic structure action table based on lead table

--- src/Components/ActionTable.js
+++ src/Components/ActionTable.js
@@ -44,33 +44,19 @@ function stableSort(array, comparator) {
 }
 
 const columns = [
-	{ id: 'company_name', label: 'Lead', minWidth: 120 },
-	{ id: 'salesCyclePhase', label: 'Verkoop fase', minWidth: 120 },
-	{ id: 'company_phone', label: 'Telefoon', minWidth: 100 },
-	{ id: 'company_email', label: 'Email', minWidth: 100 },
-	{ id: 'supplier', label: 'Leverancier', minWidth: 100 },
-	{ id: 'createdAt', label: 'Gecreëerd op', minWidth: 120 },
-	{ id: 'updatedAt', label: 'Aangepast op', minWidth: 120 },
+	{ id: 'actionTitle', label: 'Actie', minWidth: 120 },
+	{ id: 'due_date', label: 'Datum en Tijd', minWidth: 120 },
+	{ id: 'note ', label: 'Notitie', minWidth: 250 },
+	{ id: 'leadName', label: 'Lead', minWidth: 120 },
+	{ id: 'userName', label: 'Gebruiker', minWidth: 120 },
 ]
 
 const createRow = (lead) => {
-	const company_name = lead.company_name
-	const salesCyclePhase = lead.salesCyclePhase.name
-	const company_phone = lead.company_phone
-	const company_email = lead.company_email
-	const supplier = lead.supplier
-	const createdAt = moment(lead.createdAt).format('DD MMM YYYY')
-	const updatedAt = moment(lead.updatedAt).format('DD MMM YYYY')
-
-	return {
-		company_name,
-		salesCyclePhase,
-		company_phone,
-		company_email,
-		supplier,
-		createdAt,
-		updatedAt,
-	}
+    const actionTitle = action.action
+    const due_date = action.due_date
+    const note = action.note
+    const leadName = action.leadId
+    const userName = action.userId
 }
 
 const MyTableHead = (props) => {
@@ -132,10 +118,10 @@ const useStyles = makeStyles({
 	},
 })
 
-export default function StickyHeadTable(props) {
+export default function ActionTable() {
 	const classes = useStyles()
 	const dispatch = useDispatch()
-    const leads = useSelector(selectLeads)
+	const actions = useSelector(selectActions)
 	const [order, setOrder] = useState('asc')
 	const [orderBy, setOrderBy] = useState('leads')
 	const [page, setPage] = useState(0)
@@ -148,7 +134,7 @@ export default function StickyHeadTable(props) {
 	}
 
 	useEffect(() => {
-		dispatch(fetchLeads)
+		dispatch(fetchActions)
 	}, [dispatch])
 
 	const rows = leads.map((lead) => createRow(lead))
@@ -162,45 +148,5 @@ export default function StickyHeadTable(props) {
 		setPage(0)
 	}
 
-	if (!leads) {
-		return null
-	} else {
-		return (
-			<Paper className={classes.root}>
-				<TableContainer className={classes.container}>
-					<Table>
-						<MyTableHead
-							classes={classes}
-							order={order}
-							orderBy={orderBy}
-							onRequestSort={handleRequestSort}
-						/>
-						<TableBody>
-							{stableSort(rows, getComparator(order, orderBy))
-								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-								.map((row, index) => {
-									return (
-										<TableRow hover tabIndex={-1} key={row.company_name}>
-											{columns.map((column) => {
-												const value = row[column.id]
-												return <TableCell key={column.id}>{value}</TableCell>
-											})}
-										</TableRow>
-									)
-								})}
-						</TableBody>
-					</Table>
-				</TableContainer>
-				<TablePagination
-					rowsPerPageOptions={[5, 10, 25, 50]}
-					component='div'
-					count={rows.length}
-					rowsPerPage={rowsPerPage}
-					page={page}
-					onChangePage={handleChangePage}
-					onChangeRowsPerPage={handleChangeRowsPerPage}
-				/>
-			</Paper>
-		)
-	}
+	{!actions ? null : <div></div>}
 }
