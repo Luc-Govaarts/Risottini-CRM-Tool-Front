85dd64e4fde79412a36432d30fdbee0f23893451
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-08-16 14:02:08 +0200

    Ran into bug, saving changes

--- src/Pages/Actions.js
+++ src/Pages/Actions.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ActionTable from '../Components/ActionTable'
 import { Box, Typography } from '@material-ui/core'
+import PlanActionForm2 from '../Components/PlanActionForm2'
 
 export default function Actions() {
 	return (
-		<Box mt={12}>
-			<Typography variant='h3'> Acties </Typography>
-			<ActionTable />
-		</Box>
+		<>
+			<Box mt={12} mx={20}>
+				<Typography variant='h3'> Acties </Typography>
+				<ActionTable />
+			</Box>
+			<Box mx={20}>
+				<PlanActionForm2 />
+			</Box>
+		</>
 	)
 }
