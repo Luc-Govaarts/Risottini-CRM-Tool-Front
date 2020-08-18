e8b7c84934e7576cd203e386d93a12ee0b5817c2
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-08-18 18:14:33 +0200

    added button to the autocomplete contact dropdown to open dialog with contact card

--- src/Components/Navigation/SearchBar/ContactAutoComplete.js
+++ src/Components/Navigation/SearchBar/ContactAutoComplete.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Typography, Link } from '@material-ui/core'
+import { Grid, Typography, Button } from '@material-ui/core'
 import ContactPhoneIcon from '@material-ui/icons/ContactPhone'
+import moment from 'moment'
 
 const useStyles = makeStyles((theme) => ({
 	icon: {
@@ -13,22 +14,60 @@ const useStyles = makeStyles((theme) => ({
 export default function ContactAutoComplete(props) {
 	const classes = useStyles()
 	const contact = props.contact
+	const dateUpdated = moment(contact.updatedAt).format('DD MMM YYYY, hh:mm a')
+	const dateCreated = moment(contact.createdAt).format('DD MMM YYYY, hh:mm a')
+	const [open, set_open] = useState(true)
+	const [open_adjust, set_open_adjust] = useState(false)
+	const [open_delete, set_open_delete] = useState(false)
+
+	const handleOpen = () => {
+		set_open(true)
+	}
+
+	const handleClose = () => {
+		set_open(false)
+	}
+
+	const handleOpenAdjust = () => {
+		set_open_adjust(true)
+	}
+
+	const handleCloseAdjust = () => {
+		set_open_adjust(false)
+	}
+
+	const handleOpenDelete = () => {
+		set_open_delete(true)
+	}
+
+	const handleCloseDelete = () => {
+		set_open_delete(false)
+	}
+
+	// const handleAdjustContact = () => {}
+
+	// const handleDeleteContact = (event) => {
+	// 	event.preventDefault()
+
+	// 	dispatch(updateContact(null, leadId))
+	// }
 
 	return (
-		<Link href={`/contacts/${contact.id}`}>
-			<Grid container alignItems='center'>
-				<Grid item>
-					<ContactPhoneIcon className={classes.icon} />
-				</Grid>
-				<Grid item xs>
-					<Typography variant='body1' color='textPrimary'>
-						{contact.name}
-					</Typography>
-					<Typography variant='body2' color='textSecondary'>
-						{contact.job_title}
-					</Typography>
-				</Grid>
+		<Grid container alignItems='center'>
+			<Grid item>
+				<ContactPhoneIcon className={classes.icon} />
 			</Grid>
-		</Link>
+			<Grid item xs>
+				<Typography variant='body1' color='textPrimary'>
+					{contact.name}
+				</Typography>
+				<Typography variant='body2' color='textSecondary'>
+					{contact.job_title}
+				</Typography>
+				<Button onClick={handleOpen} color='primary' size='small'>
+					Meer
+				</Button>
+			</Grid>
+		</Grid>
 	)
 }
