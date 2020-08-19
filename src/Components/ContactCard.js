2d15a5a4cf3c75e6c95d47c8232097da74784b79
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-08-19 13:10:17 +0200

    contact card created for contact book

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -1,6 +1,54 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+	Typography,
+	Avatar,
+	Card,
+	CardHeader,
+	CardContent,
+} from '@material-ui/core'
+import moment from 'moment'
+
+const useStyles = makeStyles((theme) => ({
+	avatar: {
+		backgroundColor: theme.palette.secondary.main,
+	},
+}))
 
 export default function ContactCard(props) {
+	const classes = useStyles()
 	const contact = props.contact
-	return <div>{contact.name}</div>
+	const dateUpdated = moment(contact.updatedAt).format('DD MMM YYYY, hh:mm a')
+	const dateCreated = moment(contact.createdAt).format('DD MMM YYYY, hh:mm a')
+	
+	return (
+		<Card>
+			<CardHeader
+				avatar={
+					<Avatar className={classes.avatar}>
+						{contact.name.charAt(0)}
+					</Avatar>
+				}
+				title={contact.name}
+			/>
+			<CardContent>
+				<Typography>
+					<strong>Functie: </strong> {contact.job_title}
+				</Typography>
+				<Typography>
+					<strong>Email: </strong>
+					{contact.email}
+				</Typography>
+				<Typography>
+					<strong>Telefoon: </strong> {contact.phone}
+				</Typography>
+				{dateUpdated !== dateCreated ? (
+					<Typography variant='caption' color='textSecondary'>
+						{'Aangepast op: '}
+						{dateUpdated}
+					</Typography>
+				) : null}
+			</CardContent>
+		</Card>
+	)
 }
