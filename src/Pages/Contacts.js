63f35b1110a7b48cb958d9aea25a9a714e44d174
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-08-19 09:45:03 +0200

    imports, structure and basic layout of contacts page

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { fetchContacts } from '../store/appFeed/actions'
+import { selectContacts } from '../store/appFeed/selectors'
+import { selectToken } from '../store/user/selectors'
+import { useSelector, useDispatch } from 'react-redux'
+import { Box } from '@material-ui/core'
 
 export default function Contacts() {
-    return (
-        <div>
-            
-        </div>
-    )
+	const dispatch = useDispatch()
+	const contacts = useSelector(selectContacts)
+	const token = useSelector(selectToken)
+	const history = useHistory()
+
+	if (!token) {
+		history.push('/login')
+	}
+
+	useEffect(() => {
+		dispatch(fetchContacts)
+	}, [dispatch])
+
+	return (
+		<>
+			<Box mt={12} mx={25}>
+				<Grid container direction='row' justify='flex-start'>
+					<Grid item xs={6}></Grid>
+					<Grid item xs={4}></Grid>
+				</Grid>
+			</Box>
+		</>
+	)
 }
